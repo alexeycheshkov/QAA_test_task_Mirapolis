07e304eb1535b5219cfb91b43bc52a47977818b8
AlexeyCheshkov <alexeychehskov@gmail.com>
2021-08-26 12:40:44 +0500

    Full work test case.

--- src/test/java/AuthTest.java
+++ src/test/java/AuthTest.java
@@ -23,47 +23,47 @@ public class AuthTest extends BaseTest {
     @Test
     public void validCredAuthTest() {
         authForm.signIn(correctLogin, correctPass);
-        assertTrue(studentStartPage.state().waitForDisplayed());
+        assertTrue(studentStartPage.state().waitForDisplayed(), "Student start pge wasn't opened");
     }
 
     @Test
     public void invalidCredAuthTest() {
         authForm.signIn(correctLogin, incorrectPass);
-        assertEquals(browser.getDriver().switchTo().alert().getText(), wrongAuthDataAlertText);
+        assertEquals(browser.getDriver().switchTo().alert().getText(), wrongAuthDataAlertText,"Alert didn't appear");
         browser.handleAlert(AlertActions.ACCEPT);
-        assertTrue(authForm.state().isDisplayed());
+        assertTrue(authForm.state().isDisplayed(), "Alert wasn't closed");
     }
 
     @Test
     public void showPassBtnTest() {
         authForm.clickGuestPageLbl();
-        assertTrue(authForm.state().isDisplayed());
+        assertTrue(authForm.state().isDisplayed(), "Auth form wasn't opened");
         authForm.typePassword(incorrectPass);
-        assertEquals(authForm.getPassFieldAttr("type"), "password");
+        assertEquals(authForm.getPassFieldAttr("type"), "password", "Password field doesn't have 'password' type");
         authForm.clickShowPassBtn();
-        assertEquals(authForm.getPassFieldAttr("type"), "text");
+        assertEquals(authForm.getPassFieldAttr("type"), "text", "Password field doesn't have 'text' type");
     }
 
     @Test
     public void loginLengthTest() {
         authForm.typeLogin(incorrectLongLogin);
         String loginFieldText = authForm.getLoginFieldText();
-        assertNotEquals(incorrectLongLogin.length(), loginFieldText.length());
-        assertEquals(Integer.parseInt(authForm.getLoginFieldAttr("maxlength")), loginFieldText.length());
+        assertNotEquals(incorrectLongLogin.length(), loginFieldText.length(), "Sent text and text from field are equal");
+        assertEquals(Integer.parseInt(authForm.getLoginFieldAttr("maxlength")), loginFieldText.length(), "Field attribute doesn't match with text length");
     }
 
     @Test
     public void remindPassTest() {
         authForm.clickRemandPassLnk();
         remindPassForm.clickGuestPageLbl();
-        assertTrue(authForm.state().isDisplayed());
+        assertTrue(authForm.state().isDisplayed(),"Auth form wasn't opened");
         authForm.clickRemandPassLnk();
-        assertTrue(remindPassForm.state().isDisplayed());
+        assertTrue(remindPassForm.state().isDisplayed(),"Remind password form wasn't opened");
         remindPassForm.typeLoginOrEmail(incorrectLongLogin);
-        assertEquals(remindPassForm.getWrongUserAlertText(), wrongAlertForRestorePass);
+        assertEquals(remindPassForm.getWrongUserAlertText(), wrongAlertForRestorePass, "Wrong user/email alert doesn't appear");
         remindPassForm.typeLoginOrEmail(correctLogin);
-        assertEquals(remindPassForm.getSuccessUserAlertText(), successAlertForRestorePass);
+        assertEquals(remindPassForm.getSuccessUserAlertText(), successAlertForRestorePass,"Success user/email alert doesn't appear");
         remindPassForm.clickBackToAuthFormLink();
-        assertTrue(authForm.state().isDisplayed());
+        assertTrue(authForm.state().isDisplayed(),"Auth form wasn't opened");
     }
 }
